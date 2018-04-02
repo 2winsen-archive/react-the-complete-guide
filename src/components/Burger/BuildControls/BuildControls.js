bad212439d16e24d6770b17a2066237147b00ead
2winsen <vitalijs.shakels@gmail.com>
2018-04-02 21:57:46 +0300

    BuildControl

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './BuildControls.css';
+
+const BuildControls = props => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+BuildControls.propTypes = {
+
+};
+
+export default BuildControls;
