58566beebbed7131010493001dfaa129b17ed70f
2winsen <vitalijs.shakels@gmail.com>
2018-05-13 16:09:28 +0300

    Modal perf optimisaation

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Fragment from '../../../hoc/Fragment';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => {
-  return (
-    <Fragment>
-      <Backdrop show={props.show} onClick={props.onClickedOutside} />
-      <div className={classes.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}>
-        {props.children}
-      </div>
-    </Fragment>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} onClick={this.props.onClickedOutside} />
+        <div className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 export default Modal;
