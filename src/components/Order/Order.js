b473446935fcae5079db44980a635b0aa565aad9
2winsen <vitalijs.shakels@gmail.com>
2018-07-08 19:56:37 +0300

    Orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,29 @@ import React from 'react';
 
 import classes from './Order.css';
 
-const Order = () => {
+const Order = ({ ingredients, total }) => {
+  const ingredientsArr = [];
+  for (const key in ingredients) {
+    if (ingredients.hasOwnProperty(key)) {
+      ingredientsArr.push({
+        name: key,
+        amount: ingredients[key]
+      });
+    }
+  }
+
   return (
     <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
-      <p>Price: <strong>USD 4.53</strong></p>
+      <p>Ingredients: {ingredientsArr.map(i => (
+        <span style={{
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          border: '1px solid #ccc',
+          padding: '5px'
+        }} key={i.name}>{i.name}: {i.amount}</span>
+      ))}</p>
+      <p>Price: <strong>USD {parseFloat(total).toFixed(2)}</strong></p>
     </div>
   );
 };
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
   state = {
@@ -18,17 +19,22 @@ class Orders extends Component {
         }
         this.setState({ loading: false, orders });
       })
-      .catch(err => {
+      .catch(() => {
         this.setState({ loading: false });
       });
   }
 
   render() {
+    let orders =
+      this.state.orders.map(o => (
+        <Order key={o.id} ingredients={o.ingredients} total={o.total} />
+      ))
+    if (this.state.loading) {
+      orders = <Spinner />
+    }
     return (
       <div>
-        <Order />
-        <Order />
-        <Order />
+        {orders}
       </div>
     );
   }
