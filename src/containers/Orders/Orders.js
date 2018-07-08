fa826c2a86b2cbc44f1e44de7130c048cd7103c9
2winsen <vitalijs.shakels@gmail.com>
2018-07-08 19:21:29 +0300

    Orders

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Order from '../../components/Order/Order';
+
+const Orders = () => {
+  return (
+    <div>
+      <Order />
+      <Order />
+      <Order />
+    </div>
+  );
+};
+
+export default Orders;
