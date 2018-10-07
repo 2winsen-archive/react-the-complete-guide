747167967fe0cc82108aebdf7b6b730f8f8a0538
2winsen <vitalijs.shakels@gmail.com>
2018-10-07 18:17:50 +0300

    226 Improving Visual Feedback

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,7 +4,7 @@ import classes from './Input.css';
 const Input = (props) => {
   let inputElement = null;
   let inputClasses = [classes.InputElement];
-  if (props.invalid && props.shouldValidate) {
+  if (props.invalid && props.shouldValidate && props.touched) {
     inputClasses.push(classes.Invalid);
   }
 
