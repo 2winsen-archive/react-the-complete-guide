69747f12a431fda8f78fe465fc95aa1fe6e83f02
2winsen <vitalijs.shakels@gmail.com>
2018-10-28 20:05:54 +0200

    289 The Order Reducer

---
+++ src/store/reducers/order.js
@@ -0,0 +1,31 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  orders: [],
+  loading: false
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.PURCHASE_BURGER_SUCCESS: {
+      const newOrder = {
+        ...action.order,
+        id: action.id
+      };
+      return {
+        ...state,
+        loading: false,
+        orders: [...state.orders, newOrder]
+      };
+    }
+    case actionTypes.PURCHASE_BURGER_FAILED:
+      return {
+        ...state,
+        loading: false
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
