c3a337a3f88c5643b76d90bc51b3a537b2438215
2winsen <vitalijs.shakels@gmail.com>
2018-10-28 22:16:27 +0200

    297 Refactoring Reducers

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   ingredients: null,
@@ -15,36 +16,30 @@ const INGREDIENTS_PRICES = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.ADD_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-        },
+    case actionTypes.ADD_INGREDIENT: {
+      const updatedIngredients = updateObject(state.ingredients, { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 });
+      return updateObject(state, {
+        ingredients: updatedIngredients,
         totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName]
-      }
-    case actionTypes.REMOVE_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-        },
+      });
+    }
+    case actionTypes.REMOVE_INGREDIENT: {
+      const updatedIngredients = updateObject(state.ingredients, { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 });
+      return updateObject(state, {
+        ingredients: updatedIngredients,
         totalPrice: state.totalPrice - INGREDIENTS_PRICES[action.ingredientName]
-      }
+      });
+    }
     case actionTypes.SET_INGREDIENTS:
-      return {
-        ...state,
+      return updateObject(state, {
         ingredients: action.ingredients,
         totalPrice: 4,
         error: false
-      }
+      });
     case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return {
-        ...state,
+      return updateObject(state, {
         error: true
-      }
+      });
     default:
       return state;
   }
